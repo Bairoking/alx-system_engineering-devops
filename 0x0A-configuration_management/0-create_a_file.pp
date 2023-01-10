c5b111366a2e6032d0ceb4c59b7c7b16c254ae3c
Bairoking <gs8004rtj5jj5j@gmail.com>
2023-01-10 13:47:28 -0800

    Task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#This manifest create a file at /tmp as required
+
+file {'/tmp/school':
+	ensure => 'file',
+	owner => 'www-data',
+	group => 'www-data',
+	mode => '0744',
+	content => 'I Love Puppet',
+}
